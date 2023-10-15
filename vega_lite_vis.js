ea8eda3309fb15b8c3df661a15d64656aa9b441c
BTapelu <127068852+BTapelu@users.noreply.github.com>
2023-10-15 15:30:49 +1100

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,17 @@
+var vg_1 = "choroplethmap1.json";
+vegaEmbed("#choroplethmap1", vg_1).then(function(result) {}).catch(console.error);
+
+var vg_2 = "choroplethmap2.json";
+vegaEmbed("#choroplethmap2", vg_2).then(function(result) {}).catch(console.error);
+
+var vg_6 = "barchart1.json";
+vegaEmbed("#barchart1", vg_6).then(function(result) {}).catch(console.error);
+
+var vg_7 = "piechart1.json";
+vegaEmbed("#piechart1", vg_7).then(function(result) {}).catch(console.error);
+
+var vg_8 = "linechart1.json";
+vegaEmbed("#linechart1", vg_8).then(function(result) {}).catch(console.error);
+
+var vg_9 = "histogram1.json";
+vegaEmbed("#histogram1", vg_9).then(function(result) {}).catch(console.error);
